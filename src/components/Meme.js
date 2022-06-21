d7c715cf9642b0dec787e95e110212c7f45fa95c
HandyF <86957456+HandyFOff@users.noreply.github.com>
2022-06-21 20:00:51 +0300

    Add Functionality 0.0.0.3

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,24 +2,50 @@ import React from "react";
 import memesData from './memesData';
 
 export default function Meme() {
-    const [memeImage, setMemeImage] = React.useState("");
+    const [meme, setMeme] = React.useState({
+        topText: "SHUT UP",
+        bottomText: "AND TAKE MY MONEY",
+        memeImage: "http://i.imgflip.com/3si4.jpg",
+    });
+
+    const [allMemeImages, setAllMemeImage] = React.useState(memesData);
+
+    console.log(meme);
 
     function getMemeImage() {
-        const memesArray = memesData.data.memes;
-        let randomNumber = Math.floor(Math.random() * memesArray.length);
-        setMemeImage(memesArray[randomNumber].url)
+        const memesArray = allMemeImages.data.memes;
+        const randomNumber = Math.floor(Math.random() * memesArray.length);
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            memeImage: url,
+        }));
+    }
+
+    function handlerChange(event) {
+        const {name, value} = event.target;
+        setMeme(prevMeme => {
+            return {
+                ...prevMeme,
+                [name]: value,
+            }
+        });
     }
 
     return (
         <section className="meme">
             <div className="meme-form">
                 <div className="meme-form__text">
-                    <input placeholder="Top text"/>
-                    <input placeholder="Bottom text"/>
+                    <input placeholder={meme.topText} name="topText" onChange={handlerChange}/>
+                    <input placeholder={meme.bottomText} name="bottomText" onChange={handlerChange}/>
                 </div>
                 <button className="btn-create__img" onClick={getMemeImage}>Get a new meme image</button>
             </div>
-            <img className="meme-image" src={memeImage}/>
+            <div className="meme-result">
+                <img className="meme-image" src={meme.memeImage}/>
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
         </section>
     )
 }
